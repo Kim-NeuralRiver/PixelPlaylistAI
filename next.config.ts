1abfd1ec764ec1acb593346878c8117e3edd37d9
Harsh Mohan <harshmohan813@gmail.com>
2024-11-26 11:49:54 +0000

    feat: add Webpack alias for @locales in next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@locales': path.resolve(__dirname, './locales'), // Add the alias for @locales
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
