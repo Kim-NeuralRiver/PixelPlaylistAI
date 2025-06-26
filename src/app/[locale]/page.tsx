6344ed8baa1a65931b60042aec777b3685e31900
Kim-NeuralRiver <kim@neuralriver.ai>
2025-06-26 14:51:20 +0100

    Button move home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -31,7 +31,13 @@ const Home: React.FC = () => {
         <h1 className="text-4xl font-bold mb-4">{t('home:title')}</h1> 
         <p className="text-xl mb-8 mx-2">{t('home:description')}</p>
         <p className="text-lg mb-4">
-          {t('home:welcome', { username: user || t('auth:usernamePlaceholder') })} 
+          <button
+            onClick={() => router.push('/recommendations')}
+            className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+          >
+            {t('home:recommendations')}
+          </button>
+
         </p>
 
         <div className="mb-8 w-full">
@@ -45,32 +51,19 @@ const Home: React.FC = () => {
 
         <div className="flex gap-4 mb-4">
           <button
-            onClick={() => router.push('/recommendations')}
-            className="bg-blue-600 text-white px-8 py-2 rounded hover:bg-blue-700"
-          >
-            {t('home:recommendations')}
-          </button>
-
-          <button
             onClick={() => router.push('/sign-up')}
-            className="bg-green-400 text-white px-6 py-2 rounded hover:bg-green-700"
+            className="bg-green-400 text-white px-2 py-2 rounded hover:bg-green-700"
           >
             {t('auth:signUp')}
           </button>
 
           <button
             onClick={() => router.push('/sign-in')}
-            className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+            className="bg-blue-600 text-white px-2 py-2 rounded hover:bg-blue-700"
           >
             {t('auth:signIn')}
           </button>
 
-          <button
-            onClick={signOut}
-            className="bg-red-500 text-white px-2 py-2 rounded hover:bg-red-600"
-          >
-            {t('auth:signOut')}
-          </button>
         </div>
 
         <LanguageChanger />
