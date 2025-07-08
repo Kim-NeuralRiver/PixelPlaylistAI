2d771156c4afa72807ab48f36e1d125acf425400
Kim-NeuralRiver <kim@neuralriver.ai>
2025-07-08 15:11:17 +0100

    contact us button fix

--- src/app/[locale]/contact-us/page.tsx
+++ src/app/[locale]/contact-us/page.tsx
@@ -80,7 +80,7 @@ export default function ContactUsPage() {
 
           <button
             type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-button-primary hover:bg-button-primaryHover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-button-secondary hover:bg-button-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
             {t('contact:sendButton')}
           </button>
